ba1dfff1be2f08bd918198c45fe71552b55f9b08
gaelbelami <ishimwegaelbelami@gmail.com>
2022-01-31 12:32:20 -0800

    Setup: Tailwind | React~typescript | Apollo done

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from "@apollo/client";
+import { client } from './apollo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
