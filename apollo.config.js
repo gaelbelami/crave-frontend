cd25d90f5bc9b28fbf808b86688feb1b4bc09683
gaelbelami <ishimwegaelbelami@gmail.com>
2022-05-17 13:58:01 -0700

    the 50th retrial

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,14 @@
+const uri =
+  process.env.NODE_ENV === "production"
+    ? "https://crave-eat-backend.herokuapp.com/graphql"
+    : "http://localhost:4000";
 module.exports = {
   client: {
     includes: ["./src/**/*.{tsx, ts}"],
     tagName: "gql",
     service: {
       name: "crave-eat-frontend",
-      url: "http://localhost:4000/graphql",
+      url: `${uri}`,
     },
   },
 };
